e1815fed8ea27606be4999f1c50c0200c07b8fc1
Chris Beaver <chris@chrisbeaver.net>
2024-01-05 18:09:42 -0500

    move build files to .gitignore

--- terminals.d.ts
+++
@@ -1,3 +0,0 @@
-export function macOSTemplate(window: any): import("lit-html").TemplateResult<1>;
-export function ubuntuTemplate(window: any): import("lit-html").TemplateResult<1>;
-//# sourceMappingURL=terminals.d.ts.map
